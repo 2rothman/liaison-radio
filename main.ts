9921a83a446be0e0f54eb62925c3a17cc2310392
2rothman <92726613+2rothman@users.noreply.github.com>
2021-10-22 16:35:08 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,32 @@
+bluetooth.onBluetoothConnected(function () {
+    basic.showIcon(IconNames.Yes)
+})
+bluetooth.onBluetoothDisconnected(function () {
+    basic.showIcon(IconNames.No)
+})
+input.onButtonPressed(Button.A, function () {
+    bluetooth.uartWriteString("Temp =")
+    bluetooth.uartWriteNumber(input.temperature())
+})
+bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+    donnee_recue = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
+    if (donnee_recue.includes("on")) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else {
+        if (donnee_recue.includes("off")) {
+            basic.clearScreen()
+        }
+    }
+})
+let donnee_recue = ""
+bluetooth.startUartService()
+basic.showIcon(IconNames.Square)
+basic.forever(function () {
+	
+})
